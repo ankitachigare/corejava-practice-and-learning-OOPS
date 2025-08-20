5cd93ab1c0e94c5739b7789e9520505af5d2cf7f
Ankita Chigare <ankitachigare@gmail.com>
2025-08-20 21:23:46 +0530

    Add files via upload

---
+++ Alphabet_Printing.java
@@ -0,0 +1,8 @@
+class Alphabet_Printing{
+    public static void main(String args[]){
+        char ch;
+        for(ch='A';ch<='Z';ch++){
+            System.out.print(ch+"  ");
+        }
+    }
+}
